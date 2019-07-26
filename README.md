e28bdb967255707883062a69f56d7fc46a46dffb
Madison Chodikov <mach5813@colorado.edu>
2019-07-26 10:41:21 -0600

    Updated README.md file

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
-# SparkFun_Alphanumeric_Display_Arduino_Library
-Arduino Library for the SparkX Qwiic Alphanumeric Display
+SparkFun Alphanumeric Display Library
+========================================
+
+[![SparkX High Precision Temperature Sensor - TMP117]()]()
+
+[*High Precision Temperature Sensor - TMP117 (Qwiic) (SPX-)*]()
+
+The SparkFun Qwiic Alphanumeric Display is a 14-segment, 4 digit alphanumeric display with the Hotlek HT16K33 LED Driver that allows you to control every segment individually through an I2C bus. 
+
+SparkFun labored with love to create this code. Feel like supporting open source hardware? 
+Buy a [board]() from SparkFun!
+
+Repository Contents
+-------------------
+
+* **/examples** - Example code to be used in the Arduino IDE.
+* **/src** - Source files for the library (.cpp, .h).
+* **keywords.txt** - Keywords from this library that will be highlighted in the Arduino IDE.
+* **libray.properties** - General library properties for the Arduino package manager
+
+Documentation
+--------------
+
+* **[Installing an Arduino Library Guide](https://learn.sparkfun.com/tutorials/installing-an-arduino-library)** - Basic information on how to install an Arduino library.
+* **[Product Repository](https://github.com/sparkfunX/Qwiic_TMP117)** - Main repository (including hardware files)
+
+License Information
+-------------------
+
+This product is _**open source**_! 
+
+Please review the LICENSE.md file for license information. 
+
+If you have any questions or concerns on licensing, please contact technical support on our [SparkFun forums](https://forum.sparkfun.com/viewforum.php?f=152).
+
+Distributed as-is; no warranty is given.
+
+- Your friends at SparkFun.
+
+_<COLLABORATION CREDIT>_
---
+++ src/SparkFun_Alphanumeric_Display.h
@@ -0,0 +1,45 @@
+/******************************************************************************
+SparkFun_Alphanumeric_Display.h
+SparkFun Alphanumeric Display Library Header File
+Madison Chodikov @ SparkFun Electronics
+Original Creation Date: July 25, 2019
+****ENTER PRODUCT PAGE URL HERE****
+
+This file prototypes the HT16K33 class, implemented in SparkFun_Alphanumeric_Display.cpp.
+
+Development environment specifics:
+	IDE: Arduino 1.8.9
+	Hardware Platform: Arduino Uno
+	Alphanumeric Display Breakout Version: 1.0.0
+
+This code is beerware; if you see me (or any other SparkFun employee) at the
+local, and you've found our code helpful, please buy us a round!
+
+Distributed as-is; no warranty is given.
+******************************************************************************/
+
+#include <Wire.h>
+#include <Arduino.h>
+
+#ifndef __SparkFun_Alphanumeric_Display_H__
+#define __SparkFun_Alphanumeric_Display_H__
+
+class HT16K33
+{
+public:
+    HT16K33(); // Constructor
+
+    bool begin(uint8_t driverAddress, TwoWire &wirePort); // Sets the address of the device and opens the Wire port for communication
+    void setBrightness(uint8_t brightLevel);              // Sets the brightness of the LED Display, can take a number 1-16
+    uint8_t getBrightness();                              // Returns the level of brightness, 1-16
+    void startOscillation();                              // Starts the oscillation of the device
+
+private:
+    TwoWire *_i2cPort = NULL; //The generic connection to user's chosen I2C hardware
+    uint8_t _deviceAddress;   // Address of
+
+    uint16_t readRegister(uint8_t reg);             // Reads 2 register bytes from sensor
+    void writeRegister(uint8_t reg, uint16_t data); // Wires single byte of data to the sensor
+};
+
+#endif
