f14f82b00c8ab2f2197218601146fe5aab4de189
madichodikov <33360984+madichodikov@users.noreply.github.com>
2019-10-29 10:11:29 -0600

    .cpp file updates

--- src/SparkFun_Alphanumeric_Display.cpp
+++ src/SparkFun_Alphanumeric_Display.cpp
@@ -65,7 +65,8 @@ bool HT16K33::begin(uint8_t sensorAddress, TwoWire &wirePort)
 	}
 
 	/* Start the Oscillation of the device */
-	Wire.beginTransmission(0x2120); // 0x2120 = The oscillation of system setup and the definition
+	Wire.beginTrasnmission(sensorAddress);
+	Wire.Write(0x2120); // 0x2120 = The oscillation of system setup and the definition
 
 	return true; //returns true if all the checks pass
 }
@@ -77,41 +78,82 @@ bool HT16K33::begin(uint8_t sensorAddress, TwoWire &wirePort)
 bool setBrightness(uint8_t brightLevel)
 {
 	uint16_t fullLevel = 0b1110000011101111
+
+	brightLevel << 8;
+	fullLevel += brightLevel;
 	Wire.beginTransmission(HT16K33_I2C_ADDRESS);
-	if(brightLevel == 0)		// 0
-		Wire.write(0xE0EF);
-	else if(brightLevel == 1)	// 1
-		Wire.write(0xE1EF);
-	else if(brightLevel == 2)	// 2
-		Wire.write(0xE2EF);
-	else if(brightLevel == 3)	// 3
-		Wire.write(0xE3EF);
-	else if(brightLevel == 4)	// 4
-		Wire.write(0xE4EF);
-	else if(brightLevel == 5)	// 5
-		Wire.write(0xE5EF);
-	else if(brightLevel == 6)	// 6
-		Wire.write(0xE6EF);
-	else if(brightLevel == 7)	// 7
-		Wire.write(0xE7EF);
-	else if(brightLevel == 8)	// 8
-		Wire.write(0xE8EF);
-	else if(brightLevel == 9)   // 9
-		Wire.write(0xE9EF);
-	else if(brightLevel == 10)  // 10
-		Wire.write(0xEAEF);
-	else if(brightLevel == 11)  // 11
-		Wire.write(0xEBEF);
-	else if(brightLevel == 12)  // 12
-		Wire.write(0xECEF);
-	else if(brightLevel == 13)  // 13
-		Wire.write(0xEDEF);
-	else if(brightLevel == 14)  // 14
-		Wire.write(0xEEEF);
-	else if(brightLevel == 15)  // 15
-		Wire.write(0xEFEF);
-	else
-		return false;
+	Wire.write(fullLevel); // Send the full level with the correct level shifted in
+	Wire.endTransmission();
+
+	// Implementation in case above doesn't work
+	// if(brightLevel == 0)		// 0
+	// 	Wire.write(0xE0EF);
+	// else if(brightLevel == 1)	// 1
+	// 	Wire.write(0xE1EF);
+	// else if(brightLevel == 2)	// 2
+	// 	Wire.write(0xE2EF);
+	// else if(brightLevel == 3)	// 3
+	// 	Wire.write(0xE3EF);
+	// else if(brightLevel == 4)	// 4
+	// 	Wire.write(0xE4EF);
+	// else if(brightLevel == 5)	// 5
+	// 	Wire.write(0xE5EF);
+	// else if(brightLevel == 6)	// 6
+	// 	Wire.write(0xE6EF);
+	// else if(brightLevel == 7)	// 7
+	// 	Wire.write(0xE7EF);
+	// else if(brightLevel == 8)	// 8
+	// 	Wire.write(0xE8EF);
+	// else if(brightLevel == 9)   // 9
+	// 	Wire.write(0xE9EF);
+	// else if(brightLevel == 10)  // 10
+	// 	Wire.write(0xEAEF);
+	// else if(brightLevel == 11)  // 11
+	// 	Wire.write(0xEBEF);
+	// else if(brightLevel == 12)  // 12
+	// 	Wire.write(0xECEF);
+	// else if(brightLevel == 13)  // 13
+	// 	Wire.write(0xEDEF);
+	// else if(brightLevel == 14)  // 14
+	// 	Wire.write(0xEEEF);
+	// else if(brightLevel == 15)  // 15
+	// 	Wire.write(0xEFEF);
+	// else
+	// 	return false;
 
 	return true;
 }
+
+/* SET BLINK RATE
+	This function will take in a number, 0-3, to set the rate of blinking
+	for the display. 
+	0 = Off
+	1 = 2 Hz
+	2 = 1 Hz
+	3 = 0.5 Hz
+*/
+uint8_t HT16K33::setBlinkRate(uint8_t rate)
+{
+	if((rate > 3) || (rate < 0)) // 
+
+	uint16_t fullBlinkRate = 0b1000000110000000 // The address starts with blinking rate off
+
+	fullBlinkRate << 8;
+	fullBlinkRate += rate;
+	Wire.beginTransmission(HT16K33_I2C_ADDRESS);
+	Wire.write(fullBlinkRate); // Send the full level with the correct level shifted in
+	Wire.endTransmission();
+
+	// Implementation in case above doesn't work
+	// if(rate == 0)		// 0
+	//    Wire.write(0x8280);
+	// else if(rate == 1)	// 1
+	// 	  Wire.write(0x8380);
+	// else if(rate == 2)	// 2
+	// 	  Wire.write(0x8480);
+	// else if(rate == 3)	// 3
+	// 	  Wire.write(0x8580);
+	// else
+	// 	  return 0;
+}
+
