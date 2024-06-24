871796a88897811227261d5cb3cd0baf0c2fe70d
Dryw Wade <dryw.wade@sparkfun.com>
2024-06-24 10:21:34 -0600

    Update license in header comments

--- examples/User Submitted/Example10_DefineChar/Example10_DefineChar.ino
+++ examples/User Submitted/Example10_DefineChar/Example10_DefineChar.ino
@@ -11,8 +11,24 @@
  * SparkFun labored with love to create this code. Feel like supporting open source hardware?
  * Buy a board from SparkFun! https://www.sparkfun.com/products/16426
  * 
- * This code is beerware; if you see me (or any other SparkFun employee) at the
- * local, and you've found our code helpful, please buy us a round!
+ * The MIT License (MIT)
+ * 
+ * Copyright (c) 2024 SparkFun Electronics
+ * 
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions: The
+ * above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software. THE SOFTWARE IS PROVIDED
+ * "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR
+ * PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN
+ * ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+ * CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  * 
  * Hardware Connections:
  * Attach Red Board to computer using micro-B USB cable.
